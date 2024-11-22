d31d4e05e33d8ccd95596a2cf8c173ec2bfe34d6
Paolo Bonzini <pbonzini@redhat.com>
2024-11-22 17:37:08 +0100

    1.75

--- build.rs
+++ build.rs
@@ -2,8 +2,8 @@ fn main() {
     // Add a custom cfg() to avoid repeated occurrences of
     // any(feature = "std", feature = "alloc)
     if cfg!(any(feature = "std", feature = "alloc")) {
-        println!("cargo::rustc-cfg=HAVE_ALLOCATION");
+        println!("cargo:rustc-cfg=HAVE_ALLOCATION");
     }
-    println!("cargo::rustc-check-cfg=cfg(HAVE_ALLOCATION)");
-    println!("cargo::rerun-if-changed=build.rs");
+    println!("cargo:rustc-check-cfg=cfg(HAVE_ALLOCATION)");
+    println!("cargo:rerun-if-changed=build.rs");
 }
